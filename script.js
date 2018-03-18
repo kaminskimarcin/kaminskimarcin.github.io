a437ff30add11d9ced6be9c70f002f32ec071a83
kaminskimarcin <33075147+kaminskimarcin@users.noreply.github.com>
2018-03-18 14:31:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'https://cryptic-stream-93817.herokuapp.com/v1/';
-  const trelloApiRoot = 'https://cryptic-stream-93817.herokuapp.com/v1/trello/';
+  const apiRoot = 'https://shrouded-brook-81622.herokuapp.com/v1/';
+  const trelloApiRoot = 'https://shrouded-brook-81622.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
